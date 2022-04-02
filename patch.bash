ce71a3b0150d127831be22ff91b74fc6066e0d6b
ZsgsDesign <zsgsdesign@gmail.com>
2022-04-02 15:02:20 +0800

    add ppa

--- patch.bash
+++ patch.bash
@@ -6,12 +6,12 @@ if test -f "$FILE"; then
     echo "Patching docservice and convert"
     echo "skipped."
 else
-    apt-get update && apt-get install -y python3-dev
+    add-apt-repository ppa:deadsnakes/ppa && apt-get update && apt-get install -y python3.6-dev
     wget https://bootstrap.pypa.io/get-pip.py
-    python3 get-pip.py
+    python3.6 get-pip.py
     pip install pycrypto
     rm -f /var/www/onlyoffice/Data/license.lic
     echo "Patching docservice and convert"
-    python3 patch.py
+    python3.6 patch.py
     echo "done."
 fi
